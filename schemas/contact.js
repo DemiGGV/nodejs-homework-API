164ea564b77b65c378e99c1a990aca5bdf117c9d
Heorhii Honcharov <0019@ukr.net>
2023-08-13 20:55:15 +0200

    fixing validation

--- schemas/contact.js
+++ schemas/contact.js
@@ -2,8 +2,10 @@ const Joi = require("joi");
 
 const contactSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string()
+    .pattern(/^[+\]?[(]?[0-9]{3,5}[)]?[-\s]?[0-9]{3,5}[-\s]?[0-9]{4,6}$/)
+    .required(),
 });
 
 module.exports = { contactSchema };
