b4bc93a143a0e044dc5b9b1b8c8741f169aebd79
Heorhii Honcharov <0019@ukr.net>
2023-08-13 16:35:15 +0200

    Work done.

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,19 @@
 const express = require("express");
+const controller = require("../../controllers/contacts");
+
+const { validateBody } = require("../../middlewares");
+const schemas = require("../../schemas/contact");
 
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
-
-router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
-
-router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
-
-router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
-
-router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.get("/", controller.listContacts);
+router.get("/:contactId", controller.getContactById);
+router.post("/", validateBody(schemas.contactSchema), controller.addContact);
+router.delete("/:contactId", controller.removeContact);
+router.put(
+  "/:contactId",
+  validateBody(schemas.contactSchema),
+  controller.updateContact
+);
 
 module.exports = router;
