fef40cd8bca97c61c33b7ab3d51205721ccaba54
Heorhii Honcharov <0019@ukr.net>
2023-08-29 11:59:57 +0200

    adding image compression

--- helpers/imageResize.js
+++ helpers/imageResize.js
@@ -1,8 +1,10 @@
 const jimp = require("jimp");
+const JIMP_QUALITY = 70;
 
 const imageResize = async (imageUri) => {
   const image = await jimp.read(imageUri);
   await image.resize(250, 250);
+  await image.quality(JIMP_QUALITY);
   await image.writeAsync(imageUri);
 };
 
